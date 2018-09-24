f89e5a80560992b8c0692de6a9aae78d350663cd
Marty Chong <marty@themesh.ca>
2018-09-24 14:48:30 -0700

    Update setup.py
    Updating pkg version requirements

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     setup_requires=['setuptools_scm'],
     install_requires=[
-        'cloud_inquisitor~=2.0.0',
+        'cloud_inquisitor~=2.0',
         'boto3~=1.4',
         'python-dateutil~=2.6.0',
         'Flask~=0.12.2',
